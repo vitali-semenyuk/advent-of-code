eb5f42a9d82041beb050b628256524e9cee7e9e8
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2021-12-24 13:27:20 +0100

    Day 24

---
+++ day24/solution.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class ALU
+  attr_reader :registers
+
+  def initialize(program)
+    @program = program
+    @registers = Hash.new(0)
+  end
+
+  def calculate(input)
+    @program.lines.each do |instruction|
+      command, *operands = instruction.split
+
+      case command
+      when 'inp'
+        @registers[operands.first] = input.shift
+      when 'add'
+        @registers[operands.first] += val(operands.last)
+      when 'mul'
+        @registers[operands.first] *= val(operands.last)
+      when 'div'
+        @registers[operands.first] /= val(operands.last)
+      when 'mod'
+        @registers[operands.first] %= val(operands.last)
+      when 'eql'
+        @registers[operands.first] = @registers[operands.first] == val(operands.last) ? 1 : 0
+      else
+        raise 'Error'
+      end
+    end
+  end
+
+  private
+
+  def val(literal)
+    Integer(literal)
+  rescue ArgumentError
+    @registers[literal]
+  end
+end
+
+alu = ALU.new(input)
+alu.calculate('13579246899999'.chars.map(&:to_i))
+
+res = 0
+
+p1 res
+
+p2 res
+
+__END__
+inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2
