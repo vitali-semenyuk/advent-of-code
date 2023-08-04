f785798abbb687a23cf76bb3307a1e5879365301
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2023-08-04 14:22:36 +0200

    2015 day 1

--- 2015/src/day1.rs
+++ 2015/src/day1.rs
@@ -8,11 +8,29 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
 }
 
 fn solve_first_part(input: &str) -> i32 {
-    0
+    input.chars().fold(0, step)
 }
 
 fn solve_second_part(input: &str) -> i32 {
-    42
+    let mut floor = 0;
+
+    for (index, instr) in input.chars().enumerate() {
+        floor = step(floor, instr);
+
+        if floor < 0 {
+            return (index + 1) as i32;
+        }
+    }
+
+    -1
+}
+
+fn step(floor: i32, instr: char) -> i32 {
+    match instr {
+        '(' => floor + 1,
+        ')' => floor - 1,
+        _ => panic!("Unexpected value"),
+    }
 }
 
 #[cfg(test)]
@@ -21,13 +39,8 @@ mod tests {
 
     use super::*;
 
-    const INPUT: &str = "
-";
-
     #[test]
     fn test_first_part() {
-        let answer = 42;
-
         assert_eq!(0, solve_first_part("(())"));
         assert_eq!(0, solve_first_part("()()"));
         assert_eq!(3, solve_first_part("((("));
@@ -39,12 +52,11 @@ mod tests {
         assert_eq!(-3, solve_first_part(")())())"));
     }
 
-    // #[test]
+    #[test]
     fn test_second_part() {
-        let answer = 42;
-
-        assert_eq!(answer, solve_second_part(INPUT))
+        assert_eq!(1, solve_second_part(")"));
+        assert_eq!(5, solve_second_part("()())"));
     }
 
-    // check_answers!(42, 42);
+    check_answers!(280, 1797);
 }
