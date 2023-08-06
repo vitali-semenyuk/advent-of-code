3c63dd80048de0360981848b02cd281318d04d67
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2023-08-06 14:47:26 +0200

    2015 day 10

--- 2015/src/day10.rs
+++ 2015/src/day10.rs
@@ -7,36 +7,85 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> usize {
+    let input = input.strip_suffix("\n").unwrap();
+
+    look_and_say_n_times(input, 40).len()
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> usize {
+    let input = input.strip_suffix("\n").unwrap();
+
+    look_and_say_n_times(input, 50).len()
+}
+
+fn look_and_say_n_times(string: &str, iterations: u32) -> String {
+    let mut string = string.to_string();
+
+    for _ in 0..iterations {
+        string = look_and_say(&string)
+    }
+
+    string
+}
+
+fn look_and_say(string: &str) -> String {
+    let chars = string.chars();
+
+    let mut groups = Vec::new();
+    let mut current_group = Vec::new();
+
+    for char in chars {
+        if !current_group.is_empty() && char != *current_group.first().unwrap() {
+            groups.push(current_group.clone());
+            current_group.clear();
+        }
+
+        current_group.push(char)
+    }
+
+    groups.push(current_group);
+
+    groups
+        .iter()
+        .map(|group| format!("{}{}", group.len(), group.first().unwrap()))
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "1
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 82350;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 1166642;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    #[test]
+    fn test_look_and_say_n_times() {
+        assert_eq!(look_and_say_n_times("1", 5), "312211");
+    }
+
+    #[test]
+    fn test_look_and_say() {
+        assert_eq!(look_and_say("1"), "11");
+        assert_eq!(look_and_say("11"), "21");
+        assert_eq!(look_and_say("21"), "1211");
+        assert_eq!(look_and_say("1211"), "111221");
+        assert_eq!(look_and_say("111221"), "312211");
+    }
+
+    check_answers!(492982, 6989950);
 }
