69f6d051c7c8d0add005b7231ca7dda7ec47bf24
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2023-08-10 10:39:06 +0200

    2016 day 3

--- 2016/src/day3.rs
+++ 2016/src/day3.rs
@@ -1,5 +1,38 @@
 use std::fmt::Display;
 
+#[derive(Debug)]
+struct Triangle(u32, u32, u32);
+
+impl From<&str> for Triangle {
+    fn from(value: &str) -> Self {
+        let mut parts = value.split_ascii_whitespace();
+
+        Self(
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+        )
+    }
+}
+
+impl From<Vec<&str>> for Triangle {
+    fn from(value: Vec<&str>) -> Self {
+        let mut parts = value.iter();
+
+        Self(
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+        )
+    }
+}
+
+impl Triangle {
+    fn is_valid(&self) -> bool {
+        self.0 + self.1 > self.2 && self.1 + self.2 > self.0 && self.2 + self.0 > self.1
+    }
+}
+
 pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     (
         Box::new(solve_first_part(input)),
@@ -7,36 +40,60 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> usize {
+    input
+        .lines()
+        .map(Triangle::from)
+        .filter(Triangle::is_valid)
+        .count()
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> usize {
+    let triangles = input
+        .lines()
+        .map(|line| line.split_ascii_whitespace().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    triangles
+        .chunks(3)
+        .map(|c| transpose(c))
+        .flatten()
+        .map(Triangle::from)
+        .filter(Triangle::is_valid)
+        .count()
+}
+
+fn transpose<'a>(matrix: &'a [Vec<&str>]) -> Vec<Vec<&'a str>> {
+    (0..matrix.len())
+        .map(|i| matrix.iter().map(|row| row[i]).collect())
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "5 10 25
+3 4 3
+3 7 5
+1 34 5
+6 8 10
+37 92 56
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 4;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 2;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    check_answers!(982, 1826);
 }
