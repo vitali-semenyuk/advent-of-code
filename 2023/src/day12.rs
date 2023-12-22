e13f531832910f97a72c2c23012f56672f9c145c
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2023-12-22 15:20:14 +0000

    2023 day 12

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -7,25 +7,66 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (springs, groups) = line.split_once(' ').unwrap();
+            let groups = groups
+                .split(',')
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<usize>>();
+            expand(springs)
+                .into_iter()
+                .map(get_damaged_groups)
+                .filter(|variant| *variant == groups)
+                .count()
+        })
+        .sum()
 }
 
 fn solve_second_part(_input: &str) -> i32 {
     0
 }
 
+fn expand(string: &str) -> Vec<String> {
+    if let Some(index) = string.chars().position(|ch| ch == '?') {
+        let mut first = string.to_string();
+        first.replace_range(index..index + 1, ".");
+        let mut second = string.to_string();
+        second.replace_range(index..index + 1, "#");
+
+        let mut result = expand(&first);
+        result.extend(expand(&second));
+        result
+    } else {
+        vec![string.to_string()]
+    }
+}
+
+fn get_damaged_groups(string: String) -> Vec<usize> {
+    string
+        .split('.')
+        .map(|g| g.len())
+        .filter(|&n| n > 0)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 21;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
@@ -33,10 +74,10 @@ mod tests {
     #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 525152;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    // check_answers!(7857, 42);
 }
