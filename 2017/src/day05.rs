b9d26dc002655ae9b2e55b0360e1f5ce1211943f
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2024-01-05 18:33:44 +0000

    2017 day 5

--- 2017/src/day05.rs
+++ 2017/src/day05.rs
@@ -1,5 +1,40 @@
 use std::fmt::Display;
 
+struct Cpu {
+    jumps: Vec<i32>,
+    ip: usize,
+}
+
+impl Cpu {
+    fn new(jumps: Vec<i32>) -> Self {
+        Self { jumps, ip: 0 }
+    }
+
+    fn run(&mut self, sophisticated_jumps: bool) -> usize {
+        let mut ticks = 0;
+        let max_address = self.jumps.len() as i32 - 1;
+
+        loop {
+            ticks += 1;
+
+            let offset = self.jumps[self.ip];
+            let new_ip = self.ip as i32 + offset;
+            if new_ip < 0 || new_ip > max_address {
+                break;
+            }
+
+            self.jumps[self.ip] += if sophisticated_jumps && offset >= 3 {
+                -1
+            } else {
+                1
+            };
+            self.ip = new_ip as usize;
+        }
+
+        ticks
+    }
+}
+
 pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     (
         Box::new(solve_first_part(input)),
@@ -7,36 +42,44 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> usize {
+    let jumps = input.lines().map(|line| line.parse().unwrap()).collect();
+    let mut cpu = Cpu::new(jumps);
+
+    cpu.run(false)
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> usize {
+    let jumps = input.lines().map(|line| line.parse().unwrap()).collect();
+    let mut cpu = Cpu::new(jumps);
+
+    cpu.run(true)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "0
+3
+0
+1
+-3
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 5;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 10;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    check_answers!(374269, 27720699);
 }
