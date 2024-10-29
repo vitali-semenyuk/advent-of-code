8fefc1a1f45b7bb0d737d3858e373b02df82a3c7
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2024-10-29 18:19:02 +0100

    2019 day 1

--- 2019/src/day01.rs
+++ 2019/src/day01.rs
@@ -7,25 +7,48 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|l| calculate_required_fuel(l.parse::<u32>().unwrap()))
+        .sum()
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|l| calculate_full_fuel(l.parse::<u32>().unwrap()))
+        .sum()
+}
+
+fn calculate_required_fuel(mass: u32) -> u32 {
+    let fuel = mass as i32 / 3 - 2;
+    fuel.max(0) as u32
+}
+
+fn calculate_full_fuel(mass: u32) -> u32 {
+    let mut total = calculate_required_fuel(mass);
+
+    if total > 0 {
+        total += calculate_full_fuel(total)
+    }
+
+    total
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "12
+14
+1969
+100756
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 34241;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
@@ -33,10 +56,27 @@ mod tests {
     #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 51331;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    #[test]
+    fn test_calculate_required_fuel() {
+        assert_eq!(calculate_required_fuel(12), 2);
+        assert_eq!(calculate_required_fuel(14), 2);
+        assert_eq!(calculate_required_fuel(1969), 654);
+        assert_eq!(calculate_required_fuel(100756), 33583);
+        assert_eq!(calculate_required_fuel(1), 0);
+    }
+
+    #[test]
+    fn test_calculate_full_fuel() {
+        assert_eq!(calculate_full_fuel(14), 2);
+        assert_eq!(calculate_full_fuel(1969), 966);
+        assert_eq!(calculate_full_fuel(100756), 50346);
+        assert_eq!(calculate_full_fuel(1), 0);
+    }
+
+    check_answers!(3465245, 5194970);
 }
