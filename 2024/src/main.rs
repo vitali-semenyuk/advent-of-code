94fe60c5b3e4bb399b671c5a1547a82d542cf1e5
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2024-12-01 08:18:52 +0100

    Year 2024

---
+++ 2024/src/main.rs
@@ -0,0 +1,76 @@
+use std::{env, fmt::Display, fs};
+
+#[macro_use]
+mod macros;
+
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+
+type SolverFunction = fn(&str) -> (Box<dyn Display>, Box<dyn Display>);
+
+const DAYS: [SolverFunction; 25] = [
+    day01::solve,
+    day02::solve,
+    day03::solve,
+    day04::solve,
+    day05::solve,
+    day06::solve,
+    day07::solve,
+    day08::solve,
+    day09::solve,
+    day10::solve,
+    day11::solve,
+    day12::solve,
+    day13::solve,
+    day14::solve,
+    day15::solve,
+    day16::solve,
+    day17::solve,
+    day18::solve,
+    day19::solve,
+    day20::solve,
+    day21::solve,
+    day22::solve,
+    day23::solve,
+    day24::solve,
+    day25::solve,
+];
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+    let day = args.next().expect("Please provide a day");
+    let day: u8 = day.parse().expect("Day should be a number");
+
+    let input =
+        fs::read_to_string(format!("../inputs/2024/day{day:0>2}.txt")).expect("No input file");
+    let solver_fn = DAYS.get((day - 1) as usize).expect("No solver function");
+
+    let (first_answer, second_answer) = solver_fn(&input);
+
+    println!("First task: {}", first_answer);
+    println!("Second task: {}", second_answer);
+}
