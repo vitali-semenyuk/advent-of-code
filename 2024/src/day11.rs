2f158fab28637b6adcb20b63e6da52dc7740683a
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2024-12-11 06:57:30 +0100

    2024 day 11

--- 2024/src/day11.rs
+++ 2024/src/day11.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     (
@@ -7,36 +7,72 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> u64 {
+    blinks(input, 25)
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> u64 {
+    blinks(input, 75)
+}
+
+fn blinks(input: &str, n: u32) -> u64 {
+    let mut stones = input
+        .split_whitespace()
+        .map(|n| (n.parse().unwrap(), 1))
+        .collect::<HashMap<u64, u64>>();
+
+    for _ in 0..n {
+        stones = blink(&stones);
+    }
+
+    stones.values().sum()
+}
+
+fn blink(stones: &HashMap<u64, u64>) -> HashMap<u64, u64> {
+    let mut result = HashMap::new();
+
+    for (stone, count) in stones.iter().flat_map(|(&stone, &count)| {
+        if stone == 0 {
+            return vec![(1, count)];
+        }
+
+        let digits = stone.ilog10() + 1;
+
+        if digits % 2 == 0 {
+            vec![
+                (stone / 10_u64.pow(digits / 2), count),
+                (stone % 10_u64.pow(digits / 2), count),
+            ]
+        } else {
+            vec![(stone * 2024, count)]
+        }
+    }) {
+        *result.entry(stone).or_insert(0) += count;
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "125 17
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 55312;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 65601038650482;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    check_answers!(209412, 248967696501656);
 }
