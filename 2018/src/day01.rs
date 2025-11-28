8655ebc13f07b11870adb05d714a0590e6d2a40b
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2025-11-28 17:10:15 +0100

    2018 day 1

--- 2018/src/day01.rs
+++ 2018/src/day01.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{collections::HashSet, fmt::Display};
 
 pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     (
@@ -7,36 +7,50 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> i32 {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).sum()
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> i32 {
+    let mut results = HashSet::new();
+    let mut sum = 0;
+
+    results.insert(sum);
+
+    for num in input.lines().map(|l| l.parse::<i32>().unwrap()).cycle() {
+        sum += num;
+
+        if !results.insert(sum) {
+            return sum;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "+1
+-2
++3
++1
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 3;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 2;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    check_answers!(547, 76414);
 }
