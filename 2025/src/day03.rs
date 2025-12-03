7c140493694e7bfb64770329d5892077b47a07dc
Vitali Semenyuk <vitali.semianiuk@gmail.com>
2025-12-03 12:05:19 +0100

    2025 day 3

--- 2025/src/day03.rs
+++ 2025/src/day03.rs
@@ -7,36 +7,60 @@ pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     )
 }
 
-fn solve_first_part(_input: &str) -> i32 {
-    0
+fn solve_first_part(input: &str) -> u64 {
+    input.lines().map(|b| calculate_joltage(b, 2)).sum()
 }
 
-fn solve_second_part(_input: &str) -> i32 {
-    0
+fn solve_second_part(input: &str) -> u64 {
+    input.lines().map(|b| calculate_joltage(b, 12)).sum()
+}
+
+fn calculate_joltage(battery: &str, n: usize) -> u64 {
+    let mut result = 0;
+    let cells = battery
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect::<Vec<_>>();
+    let len = battery.len();
+    let mut start = 0;
+
+    for i in 0..n {
+        let left = n - i - 1;
+        let end = len - left;
+        let window = &cells[start..end];
+
+        let max = window.iter().max().unwrap();
+        result = result * 10 + max;
+
+        start += window.iter().position(|c| c == max).unwrap() + 1;
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "
+    const INPUT: &str = "987654321111111
+811111111111119
+234234234234278
+818181911112111
 ";
 
-    #[ignore]
     #[test]
     fn test_first_part() {
-        let answer = 42;
+        let answer = 357;
 
         assert_eq!(answer, solve_first_part(INPUT))
     }
 
-    #[ignore]
     #[test]
     fn test_second_part() {
-        let answer = 42;
+        let answer = 3121910778619;
 
         assert_eq!(answer, solve_second_part(INPUT))
     }
 
-    // check_answers!(42, 42);
+    check_answers!(16854, 167526011932478);
 }
